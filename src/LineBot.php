07008a02cf0d03fd9f8b6ed2a96e1011a7c5f3f2
arm <moungnoi_k@silpakorn.edu>
2020-10-21 19:36:12 +0700

    update version text

--- src/LineBot.php
+++ src/LineBot.php
@@ -1,127 +1,128 @@
 <?php
 namespace ArmKrit;
-class LineBot {
+
+class LineBot
+{
     private $token;
     private $secretId;
 
     private $replyToken;
     private $uid;
-    private $str = NULL;
+    private $str = null;
 
     private $messages = [];
 
-    function __construct($token, $secretId) {
+    public function __construct($token, $secretId)
+    {
         $this->token = $token;
         $this->secretId = $secretId;
     }
 
-    public function __call($name,$arg){
-        call_user_func($name,$arg);
+    public function __call($name, $arg)
+    {
+        call_user_func($name, $arg);
     }
 
-    function setUser($replyToken, $uid) {
+    public function setUser($replyToken, $uid)
+    {
         $this->replyToken = $replyToken;
         $this->uid = $uid;
         return $this;
     }
 
-    public function addText($text){
-       $this->messages[] = [
-                    "type" => "text",
-                    "text" => "{$text}"
-        ];
-        return $this;
-    }
+    public function addCarousel($name, $contents = [])
+    {
 
-    public function addCarousel($name, $contents = []){
-
-        $this->messages[] =  [
-                "type" => "flex",
-                "altText" => $name,
-                "contents" => array(
-                    'type' => 'carousel',
-                    'contents' => $contents,
-                ),
+        $this->messages[] = [
+            "type" => "flex",
+            "altText" => $name,
+            "contents" => array(
+                'type' => 'carousel',
+                'contents' => $contents,
+            ),
         ];
 
         return $this;
 
     }
 
-    public function addTexts($tests = []){
+    public function addText($text)
+    {
 
-        foreach($tests as $text){
+        foreach ((array) $text as $key => $text) {
             $this->messages[] = [
                 "type" => "text",
-                "text" => "{$text}"
+                "text" => "{$text}",
             ];
         }
 
         return $this;
     }
 
-    public function push(){
+    public function push()
+    {
 
         $data = [
             "to" => $this->uid,
-            "messages" => $this->messages
+            "messages" => $this->messages,
         ];
 
         return $this->sendPushMessage($data);
     }
 
-    public function reply(){
+    public function reply()
+    {
 
         $data = [
             "replyToken" => $this->replyToken,
-            "messages" => $this->messages
+            "messages" => $this->messages,
 
         ];
 
         return $this->sendReplyMessage($data);
     }
 
-     public function getJson(){
+    public function getJson()
+    {
 
         $data = [
             "to" => $this->uid,
             "replyToken" => $this->replyToken,
-            "messages" => $this->messages
+            "messages" => $this->messages,
         ];
 
         return json_encode($data, JSON_UNESCAPED_UNICODE);
     }
 
-
-     public function getUID(){
+    public function getUID()
+    {
         return $this->uid;
     }
 
-
     private function sendPushMessage($data)
     {
-      try {
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+        try {
+            $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-        $url = 'https://api.line.me/v2/bot/message/push';
-        $accssToken = $this->token;
-        $post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $accssToken);
+            $url = 'https://api.line.me/v2/bot/message/push';
+            $accssToken = $this->token;
+            $post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $accssToken);
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return $result;
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            return $result;
         } catch (\Exception $e) {
             return $e;
         }
     }
 
-        public function sendReplyMessage($data)
+    public function sendReplyMessage($data)
     {
         try {
             $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
